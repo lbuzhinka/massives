b0ac4c1d0d13988ec123c78bae4ae6d0810c1966
lbuzhinka <lbuzhinka@gmail.com>
2024-05-10 11:09:22 +0300

    Исправила задачу 4

--- src/Main.java
+++ src/Main.java
@@ -52,9 +52,9 @@ public class Main {
         System.out.println("\nTask 4");
 
         for (int i = 0; i < arr.length; i++)
-            if (arr[i] % 2 != 0)
+        {if (arr[i] % 2 != 0)
                 arr [i] ++;
-                System.out.println(Arrays.toString(arr));
+                System.out.println(Arrays.toString(arr));}
     }}
 
 
