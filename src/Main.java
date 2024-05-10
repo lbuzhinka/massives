6583bb098f0228379cf54d1d2c71a47f5e3d264e
lbuzhinka <lbuzhinka@gmail.com>
2024-05-10 14:53:38 +0300

    Исправила снова

--- src/Main.java
+++ src/Main.java
@@ -51,10 +51,11 @@ public class Main {
         }
         System.out.println("\nTask 4");
 
-        for (int i = 0; i < arr.length; i++)
-        {if (arr[i] % 2 != 0)
-                arr [i] ++;
-                System.out.println(Arrays.toString(arr));}
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) {
+                arr [i] ++;}
+                System.out.println(Arrays.toString(arr));
+        }
     }}
 
 
